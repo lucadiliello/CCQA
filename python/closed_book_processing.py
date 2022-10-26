bb07724b207f6c0528aa80c04669cfd03c80e5fc
lucadiliello <luca.diliello@unitn.it>
2022-10-26 22:36:23 +0200

    refactored closed_book_processing with multiprocessing and folder processing

--- python/closed_book_processing.py
+++ python/closed_book_processing.py
@@ -4,75 +4,113 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-import argparse
+from argparse import ArgumentParser, Namespace
+from functools import partial
 import html
 import json
 import os
 import re
-
+from typing import Dict, Tuple
+from multiprocessing import Pool
+from tqdm import tqdm
 from lxml import etree
 
 
-def extract_text(input_text, keep_markup):
+def extract_text(input_text: str, keep_markup: bool = False):
+    r""" Clean text from markup and other junk. """
     input_text = input_text.replace("\n", "").replace("\r", "")
+
     if keep_markup:
         text = html.unescape(input_text)
+
     elif not keep_markup:
         text_root = etree.HTML(input_text)
+
         if text_root is None:
             return None
+
         text = " ".join(text_root.itertext())
         text = re.sub(" +", " ", text)
         text = text.encode("ascii", "xmlcharrefreplace").decode("utf-8")
         text = html.unescape(text)
+
     return text
 
-def generate_closed_book_format(data_path, only_english, keep_markup, output_path):
-    with open(data_path, "r") as f:
-        question_list = []
-        answer_list = []
-        for website in f:
+
+def generate_closed_book_format(paths: Tuple[str], only_english: bool = True, keep_markup: bool = False) -> Dict:
+    r""" Process a single json file and output dictionary of question and answers. """
+    input_path, output_path = paths
+
+    with open(input_path, "r") as fi:
+        output = dict()
+
+        for website in fi:
             content = json.loads(website)
+
             if only_english and content["Fasttext_language"] != "en":
                 continue
+
             questions = content["Questions"]
+
             for question in questions:
                 question_text = ""
+                answer_list = []
+
                 if "name_markup" in question.keys():
-                    extracted_text = extract_text(question["name_markup"], keep_markup)
+                    extracted_text = extract_text(question["name_markup"], keep_markup=keep_markup)
                     if extracted_text is not None:
                         question_text += extracted_text + " "
+
                 if "text_markup" in question.keys():
-                    extracted_text = extract_text(question["text_markup"], keep_markup)
+                    extracted_text = extract_text(question["text_markup"], keep_markup=keep_markup)
                     if extracted_text is not None:
                         question_text += extracted_text
+
                 if len(question_text) > 0:
                     for answer in question["Answers"]:
                         if "text_markup" in answer.keys():
                             answer_text = extract_text(
-                                answer["text_markup"], keep_markup
+                                answer["text_markup"], keep_markup=keep_markup
                             )
-                        if (
-                            answer_text is not None
-                            and len(answer_text.replace("\n", "").replace("\r", "")) > 0
-                        ):
-                            question_list.append(question_text)
+                        else:
+                            answer_text = None
+
+                        if answer_text:
                             answer_list.append(answer_text)
 
-    with open(output_path + ".source", "w") as f:
-        for element in question_list:
-            f.write(element.replace("\n", "").replace("\r", "") + "\n")
-    with open(output_path + ".target", "w") as f:
-        for element in answer_list:
-            f.write(element.replace("\n", "").replace("\r", "") + "\n")
+                if question_text and answer_list:
+                    output[question_text] = answer_list
+
+    with open(output_path, "w") as fo:
+        json.dump(output, fo)
+
+
+def main(args: Namespace):
+
+    assert os.path.isdir(args.input_folder)
+    assert not os.path.isdir(args.output_folder)
+
+    os.makedirs(args.output_folder, exist_ok=False)
+
+    files = []
+    for filename in os.listdir(args.data_folder):
+        files.append((
+            os.path.join(args.input_folder, filename),
+            os.path.join(args.output_folder, filename),
+        ))
+
+    pool = Pool(args.num_workers)
+    fn_worker = partial(generate_closed_book_format, only_english=args.only_english, keep_markup=args.keep_markup)
+    
+    tqdm(pool.map(fn_worker, files), desc="Processing...", total=len(files))
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
+    parser = ArgumentParser(
         description="Generate sequence-to-sequence input and output for closed-book QA"
     )
-    parser.add_argument("--data_path", help="Path to the json dataset")
-    parser.add_argument("--output_path", help="Path to the output file")
+    parser.add_argument("--input_folder", help="Path to the json dataset")
+    parser.add_argument("--output_folder", help="Path to the output file")
     parser.add_argument(
         "--only_english",
         action="store_true",
@@ -80,6 +118,9 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--keep_markup", action="store_true", help="Keep the HTML markup"
+    )
+    parser.add_argument(
+        "--num_workers", type=int, default=1, help="Use multiprocessing"
     )
     args = parser.parse_args()
     generate_closed_book_format(
